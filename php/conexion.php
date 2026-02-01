d182272c833ec03bbc7540a5d5998ada885712da
Johana Zavaleta <zavaletaperez70@gmail.com>
2026-02-01 21:05:31 +0000

    desarrollo php

---
+++ php/conexion.php
@@ -0,0 +1,14 @@
+<?php
+// conexion.php
+
+$host = "localhost";
+$usuario = "usuario_bd";
+$contrasena = "password_bd";
+$base_datos = "proyecto_daw";
+
+$conexion = new mysqli($host, $usuario, $contrasena, $base_datos);
+
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
+?>
